9678fe1499c2667a1a36a9dac7c9d8abb4da5f3f
nathangiose <nathangiose@gmail.com>
2021-11-02 10:15:18 +0200

    nodemon and npm routing

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ const hostname = 'localhost';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+    // console.log(req);
+    const translations = {1: 'one', 2: 'two', 3:'three'};
+
+    const { url } = req;
+
+    console.log(url);
+
+    if (url === '/translations') {
+        res.setHeader('Content-Type', 'application/json');
+
+        res.write(JSON.stringify(translations));
+
+        res.end();
+    }
+
     res.end('Welcome to Node!');
 });
 
